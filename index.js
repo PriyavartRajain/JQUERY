e4555185a63b57e35b1614b95d784bcbc318422f
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2022-02-16 02:21:36 -0700

    JQuery - Basic Events

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* SELECTORS */
+
 /* Selecting with tag name. All h1's will hide */
 // $('h1').hide();
 
@@ -27,3 +29,96 @@ li: even refers to every item at an even place, and li: odd refers to every item
 
 /* Selecting with type and attribute. Every a tag with https://yahoo.com as a href value will turn green*/
 // $('a[href="https://yahoo.com"]').css('color', 'green')
+
+/* EVENTS */
+
+/* Adding a click event listener to an element by selecting it using its id*/
+// $('#btn1').click(function () {
+//     alert('Button Clicked!');
+// });
+
+/* Another way to do it: */
+// $('#btn1').on('click', function () {
+//     alert('Button Clicked');
+// });
+
+/*Using click event listener to do some other fun stuff, like making other elements disappear */
+// $('#btn1').click(function () {
+//     $('.para1').hide();
+// })
+
+/*Or.. Make it re-appear */
+// $('#btn2').click(function () {
+//     $('.para1').show();
+// })
+
+/*We could use toggle method to shorten the above code */
+// $('#btn1').click(function () {
+//     $('.para1').toggle();
+// })
+
+/*More events... */
+/*Double click - dblclick*/
+// $('#btn1').dblclick(function () {
+//     $('.para1').toggle();
+// });
+
+/*Hover (a combination of mouseenter and mouseleave functions)*/
+// $('#btn1').hover(function () {
+//     $('.para1').toggle();
+// });
+
+/*Mouse move (If the mouse moves within the element) */
+// $('#btn1').mousemove(function () {
+//     $('.para1').toggle();
+// });
+
+/*Accessing event details by passing e to the event function */
+/*The following prints all the details, e.g, timestamp, event type, etc. on the console */
+// $('#btn1').click(function (e) {
+//     console.log(e);
+// });
+
+/*We can use the object stored in e to retrieve information like id of the button*/
+/*This prints: btn1 */
+// $('#btn1').click(function (e) {
+//     console.log(e.currentTarget.id)
+// });
+
+/*Cool stuff with mousemove */
+/*The following prints the current coordinates of the mouse on the console */
+// $(document).mousemove(function (e) {
+//     console.log('X: ' + e.clientX + ' Y: ' + e.clientY);
+// })
+
+/*We can even print these coordinates on the browser itself */
+// $(document).mousemove(function (e) {
+//     $('#coords').html('X: ' + e.clientX + ' Y: ' + e.clientY);
+// })
+
+/*Focus event - When you click into an input element*/
+// $('input').focus(function () {
+//     $(this).css('background', 'pink');
+// })
+
+/*Blue event:  Opposite of focus - When you want the focus to go away when you click outside the element*/
+// $('input').blur(function () {
+//     $(this).css('background', 'white');
+// })
+
+/*keyup event: when you want to fetch what is being typed in a field */
+// $('input').keyup(function (e) {
+//     console.log(e.target.value);
+// })
+
+/*change event: when the value in a field (or in this case select) changes*/
+// $('select#gender').change(function () {
+//     alert('Changed');
+// })
+
+/*submit event: when a form is submitted */
+// $('#form').submit(function (e) {
+//     e.preventDefault();               // Prevents the form from submitting right away
+//     let name = $('input#name').val(); // Grabs the value of the input field with id="name"
+//     console.log(name);
+// })
